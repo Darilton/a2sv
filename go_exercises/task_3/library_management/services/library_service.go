b4d026a5c733356073a1c347cf74fdd29c958fcc
gumball <dariltonmario14@gmail.com>
2025-10-29 21:00:36 +0100

    Implement listing of available books

--- go_exercises/task_3/library_management/services/library_service.go
+++ go_exercises/task_3/library_management/services/library_service.go
@@ -4,6 +4,7 @@ import "library_management/models"
 
 type LibraryManager interface {
 	AddBook(book models.Book)
+	ListAvailableBooks() []models.Book
 }
 
 type Library struct {
@@ -12,4 +13,14 @@ type Library struct {
 
 func (l Library) AddBook(book models.Book) {
 	l.Books[book.ID] = book
+}
+
+func (l Library) ListAvailableBooks() []models.Book {
+	ans := make([]models.Book, 0)
+	for _, book := range l.Books {
+		if book.Status == "Available" {
+			ans = append(ans, book)
+		}
+	}
+	return ans
 }
